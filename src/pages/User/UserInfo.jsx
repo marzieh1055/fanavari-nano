6789724e0fdbaa0b648bcebc0d0bca000c61e42b
Ali <ali.moaydi2020@gmail.com>
2023-07-04 02:13:16 +0330

    info for admin and expert

--- src/pages/User/UserInfo.jsx
+++ src/pages/User/UserInfo.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { RiPencilLine } from "react-icons/ri";
 import user from "../../assets/imges/user.png"
+import Axios from "../../../axiosinstancs";
+import { UserDataContext } from "../../contexts/UserData.Provider";
 export default function UserInfo() {
+  const [details , setDetails] = useState()
+  const { userDatas } = useContext(UserDataContext)
+  useEffect(() => {
+    Axios.get("/api/v1/profile_genuine")
+    .then((res) => {
+      console.log(res);
+      const newA = res.data.reverse()
+      newA.map((item) => {
+        if(item.user.id === userDatas.user.id) {
+          setDetails(item)
+          console.log(item);
+        }
+      })
+    })
+    .catch((err) => {
+     console.log(err); 
+    })
+  } , [])
+
   return (
     <div className="bg-white rounded-2xl mt-6 p-6">
       <div className=" p-6">
@@ -11,12 +32,13 @@ export default function UserInfo() {
       <div className="">
         <div className="w-1/2 mx-auto">
           <div className="flex mt-6 items-center">
-            <img src={user} alt="" className="w-16" />
+            {
+              details && details.image && <img style={{borderRadius : "50%"}} src={`https://backend.nanotf.ir/${details.image}`} alt="عکس پروفایل" className="w-16 h-16" />
+            }
+            {(!details || !details.image) && <img src={user}  alt="" className="w-16 h-16 " />}
+            
             <div className=" pr-4">
               <p className="font-bold">عکس پروفایل</p>
-              <p className="text-gray-500 text-xs">
-                فرمت های jpg و png با حداکثر حجم 800kb
-              </p>
             </div>
           </div>
           <div className="mt-6">
@@ -24,54 +46,29 @@ export default function UserInfo() {
               <p className="font-bold text-sm">نام نام خانوادگی</p>
               <RiPencilLine className="mr-3"/>
             </div>
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="ثبت نشده"
-              className="border-0 rounded-xl bg-purple-50 outline-none mt-2 w-full"
-            />
+            <p className="border-0 w-[460px] rounded-xl p-2 bg-purple-50 outline-none mt-2 w-full">{`${userDatas.user.name} ${userDatas.user.family}`}</p>
           </div>
           <div className="mt-6">
             <div className="flex">
               <p className="font-bold text-sm">شماره تلفن</p>
               <RiPencilLine className="mr-3" />
             </div>
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="09924501564789191"
-              className="border-0 rounded-xl bg-purple-50 outline-none mt-2 w-full"
-            />
+            <p className="border-0 w-[460px] rounded-xl p-2 bg-purple-50 outline-none mt-2 w-full">{userDatas.user.phone}</p>
           </div>
           <div className="mt-6">
             <div className="flex">
               <p className="font-bold text-sm">ایمیل</p>
               <RiPencilLine className="mr-3" />
             </div>
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="ranjbarmohad@gmail.com"
-              className="border-0 rounded-xl bg-purple-50 outline-none mt-2 w-full"
-            />
+            <p className="border-0 w-[460px] rounded-xl p-2 bg-purple-50 outline-none mt-2 w-full">{userDatas.user.email}</p>
           </div>
           <div className="mt-6">
             <div className="flex">
               <p className="font-bold text-sm">کد ملی</p>
               <RiPencilLine className="mr-3" />
             </div>
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="0216548671589"
-              className="border-0 rounded-xl bg-purple-50 outline-none mt-2 w-full"
-            />
+            <p className="border-0 w-[460px] rounded-xl p-2 bg-purple-50 outline-none mt-2 w-full">{userDatas.user.national_code}</p>
           </div>
-          <button className="rounded-xl p-3 px-8 mt-6 bg-blue-800 text-white text-sm ">ذخیره</button>
         </div>
       </div>
     </div>
