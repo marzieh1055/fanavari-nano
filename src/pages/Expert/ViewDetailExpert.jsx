e26cf205339f249e0287e1ef94ae8e86fbb251be
Ali <ali.moaydi2020@gmail.com>
2023-08-19 00:00:58 +0330

    ViewDetailExpert

--- src/pages/Expert/ViewDetailExpert.jsx
+++ src/pages/Expert/ViewDetailExpert.jsx
@@ -34,85 +34,45 @@ const ViewDetailExpert = () => {
       <div className="flex flex-wrap">
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">نام و نام خانوادگی</p>
-          <input
-            type="text"
-            placeholder={`${details.name} ${details.family}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.name} ${details.family}`}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">محل صدور</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.place_issue}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.place_issue}`}</p>
         </div>
 
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">نام پدر</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.father_name}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.father_name}`}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">کد ملی</p>
-          <input
-            type="text"
-            placeholder={details.national_code}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{details.national_code}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">تاریخ تولد</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.birth_day}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.birth_day}`}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">شماره شناسنامه</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.number_certificate}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.number_certificate}`}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">سمت</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.job}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.job}`}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">تلفن همراه</p>
-          <input
-            type="text"
-            placeholder={`${details.phone}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.phone}`}</p>
         </div>
         
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">میزان تحصیلات</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.education}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.education}`}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">رشته تحصیلی</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.study}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.study}`}</p>
         </div>
       </div>
       <p className="text-xl font-bold p-4 py-6">محل سکونت</p>
@@ -121,36 +81,20 @@ const ViewDetailExpert = () => {
       <div className="flex flex-wrap">
       <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">تلفن ثابت</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.address.home_number}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.address.home_number}`}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">آدرس</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.address.address}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.address.address}`}</p>
         </div>
 
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">کد پستی</p>
-          <input
-            type="text"
-            placeholder={`${details.profilegenuine.address.postal_code}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.profilegenuine.address.postal_code}`}</p>
         </div>
         <div className="mt-3 w-96 border rounded-2xl mx-3 p-2 overflow-hidden">
           <p className="font-bold text-sm">پست الکترونیک</p>
-          <input
-            type="text"
-            placeholder={`${details.email}`}
-            className="outline-none border-none placeholder:text-sm"
-          />
+          <p className="text-gray-500">{`${details.email}`}</p>
         </div>
       </div>
     </form>
