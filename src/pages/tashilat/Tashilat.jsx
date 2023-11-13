61658435eee5132f002020734bb43434d94133ce
alim1381 <ali.moaydi2020@gmail.com>
2023-11-13 22:13:50 +0330

    change text color in tashilat

--- src/pages/tashilat/Tashilat.jsx
+++ src/pages/tashilat/Tashilat.jsx
@@ -6,51 +6,54 @@ import Loader from "../../components/Loader/Loader";
 
 export default function Tashilat() {
   let { pathname: pass } = useLocation();
-  const [stepLevel , setStepLevel] = useState(pass.split("/")[3])
+  const [stepLevel, setStepLevel] = useState(pass.split("/")[3]);
   console.log(stepLevel);
 
-  const {userDatas} = useContext(UserDataContext)
-  const navigate = useNavigate()
-  const [isLoading , setIsLoading] = useState(true)
+  const { userDatas } = useContext(UserDataContext);
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setStepLevel(pass.split("/")[3])
+    setStepLevel(pass.split("/")[3]);
     if (pass === "/panel/tashilat" || pass === "/panel/tashilat/") {
-      navigate("/panel/tashilat/1")
+      navigate("/panel/tashilat/1");
     }
-  } , [pass])
+  }, [pass]);
   useEffect(() => {
     Axios.get("/api/v1/is_profile_genuine")
-    .then((res) => {
-      console.log(res);
-      setIsLoading(false)
-      if (!res.data && userDatas.user.type === "genuine") {
-        navigate(`/panel/genuineUserInfo`)
-      }
-    })
-    .catch((err) =>{
-      console.log(err);
-      navigate(`/panel/404`)
-    })
+      .then((res) => {
+        console.log(res);
+        setIsLoading(false);
+        if (!res.data && userDatas.user.type === "genuine") {
+          navigate(`/panel/genuineUserInfo`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        navigate(`/panel/404`);
+      });
     Axios.get("/api/v1/is_profile_legal")
-    .then((res) => {
-      console.log(res);
-      setIsLoading(false)
-      if (!res.data && userDatas.user.type === "legal") {
-        navigate(`/panel/legaluserInfo`)
-      }
-    })
-    .catch((err) =>{
-      console.log(err);
-      navigate(`/panel/404`)
-    })
-
-  } , [])
-  if (isLoading) return <Loader />
+      .then((res) => {
+        console.log(res);
+        setIsLoading(false);
+        if (!res.data && userDatas.user.type === "legal") {
+          navigate(`/panel/legaluserInfo`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        navigate(`/panel/404`);
+      });
+  }, []);
+  if (isLoading) return <Loader />;
   return (
     <div className="px-5">
       <div className=" py-6">
         <p className="text-xl font-extrabold">بارگیری و بارگذاری مدارک </p>
+        <p className=" text-yellow-400">
+          کاربر عزیز، شما موظف به تکمیل تمامی صفحات برای نهایی کردن درخواست خود
+          می باشید، در غیر این صورت درخواست شما بررسی نخواهد شد.
+        </p>
       </div>
       <div className=" flex  items-center w-full justify-center">
         <div className="flex items-center">
@@ -59,50 +62,122 @@ export default function Tashilat() {
           </p>
         </div>
         <div className="w-10 px-2">
-          <div className={stepLevel > 1 || stepLevel === "confirm" ? "border-t border-2 border-blue-800 h-full rounded" : "border-t border-2 border-slate-300 h-full rounded"}></div>
+          <div
+            className={
+              stepLevel > 1 || stepLevel === "confirm"
+                ? "border-t border-2 border-blue-800 h-full rounded"
+                : "border-t border-2 border-slate-300 h-full rounded"
+            }
+          ></div>
         </div>
         <div className="flex items-center">
-          <p className={stepLevel >= 2  || stepLevel === "confirm"? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 " : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "}>
+          <p
+            className={
+              stepLevel >= 2 || stepLevel === "confirm"
+                ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+                : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+            }
+          >
             2
           </p>
         </div>
         <div className="w-10 px-2">
-          <div className={stepLevel > 2 || stepLevel === "confirm" ? "border-t border-2 border-blue-800 h-full rounded" : "border-t border-2 border-slate-300 h-full rounded"}></div>
+          <div
+            className={
+              stepLevel > 2 || stepLevel === "confirm"
+                ? "border-t border-2 border-blue-800 h-full rounded"
+                : "border-t border-2 border-slate-300 h-full rounded"
+            }
+          ></div>
         </div>
         <div className="flex items-center">
-          <p className={stepLevel >= 3  || stepLevel === "confirm"? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 " : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "}>
+          <p
+            className={
+              stepLevel >= 3 || stepLevel === "confirm"
+                ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+                : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+            }
+          >
             3
           </p>
         </div>
         <div className="w-10 px-2">
-          <div className={stepLevel > 3 || stepLevel === "confirm" ? "border-t border-2 border-blue-800 h-full rounded" : "border-t border-2 border-slate-300 h-full rounded"}></div>
+          <div
+            className={
+              stepLevel > 3 || stepLevel === "confirm"
+                ? "border-t border-2 border-blue-800 h-full rounded"
+                : "border-t border-2 border-slate-300 h-full rounded"
+            }
+          ></div>
         </div>
         <div className="flex items-center">
-          <p className={stepLevel >= 4 || stepLevel === "confirm" ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 " : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "}>
+          <p
+            className={
+              stepLevel >= 4 || stepLevel === "confirm"
+                ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+                : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+            }
+          >
             4
           </p>
         </div>
         <div className="w-10 px-2">
-          <div className={stepLevel > 4 || stepLevel === "confirm" ? "border-t border-2 border-blue-800 h-full rounded" : "border-t border-2 border-slate-300 h-full rounded"}></div>
+          <div
+            className={
+              stepLevel > 4 || stepLevel === "confirm"
+                ? "border-t border-2 border-blue-800 h-full rounded"
+                : "border-t border-2 border-slate-300 h-full rounded"
+            }
+          ></div>
         </div>
         <div className="flex items-center">
-          <p className={stepLevel >= 5 || stepLevel === "confirm" ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 " : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "}>
+          <p
+            className={
+              stepLevel >= 5 || stepLevel === "confirm"
+                ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+                : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+            }
+          >
             5
           </p>
         </div>
         <div className="w-10 px-2">
-          <div className={stepLevel > 5 || stepLevel === "confirm" ? "border-t border-2 border-blue-800 h-full rounded" : "border-t border-2 border-slate-300 h-full rounded"}></div>
+          <div
+            className={
+              stepLevel > 5 || stepLevel === "confirm"
+                ? "border-t border-2 border-blue-800 h-full rounded"
+                : "border-t border-2 border-slate-300 h-full rounded"
+            }
+          ></div>
         </div>
         <div className="flex items-center">
-          <p className={stepLevel >= 6 || stepLevel === "confirm" ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 " : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "}>
+          <p
+            className={
+              stepLevel >= 6 || stepLevel === "confirm"
+                ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+                : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+            }
+          >
             6
           </p>
         </div>
         <div className="w-10 px-2">
-          <div className={stepLevel === "confirm" ? "border-t border-2 border-blue-800 h-full rounded" : "border-t border-2 border-slate-300 h-full rounded"}></div>
+          <div
+            className={
+              stepLevel === "confirm"
+                ? "border-t border-2 border-blue-800 h-full rounded"
+                : "border-t border-2 border-slate-300 h-full rounded"
+            }
+          ></div>
         </div>
         <div className="flex items-center">
-          <p className={stepLevel === "confirm" ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 " : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "} >
+          <p
+            className={
+              stepLevel === "confirm"
+                ? "bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+                : "bg-slate-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 "
+            }
+          >
             تایید
           </p>
         </div>
